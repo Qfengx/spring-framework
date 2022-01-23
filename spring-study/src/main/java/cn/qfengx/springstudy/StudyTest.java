f282b04ab502c5b63335159c470bf3553a65ada3
qfengx <qffg1998@126.com>
2022-01-23 21:19:08 +0800

    update

---
+++ spring-study/src/main/java/cn/qfengx/springstudy/StudyTest.java
@@ -0,0 +1,13 @@
+package cn.qfengx.springstudy;
+
+/**
+ * @author Qfengx <qffg1998@126.com>
+ * @date 2022/1/23 20:23
+ */
+public class StudyTest {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+	}
+
+}
